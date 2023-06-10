41e62e1564b0bb49da97ebbcc5732704b246bd43
Abrorxon-abi <ppvbuyppv@gmail.com>
2023-06-10 22:51:11 +0500

    new price

--- src/components/sections/headingPages/Specials.js
+++ src/components/sections/headingPages/Specials.js
@@ -13,7 +13,7 @@ export default function Specials() {
             </article>
 
             <section className="specials-cards">
-                <SpecialCard image={GreekSalad} name="Greek Salad" price="$12.99" description="Refreshing salad, made with tomato, lettuce, feta cheese, and olives.
+                <SpecialCard image={GreekSalad} name="Greek Salad" price="$12.69" description="Refreshing salad, made with tomato, lettuce, feta cheese, and olives.
                  Dressed with salt, hot pepper, and olive oil."/>
                 <SpecialCard image={Bruschetta}name="Bruschetta" price="$16.99" description="Toasted bread, topped with tomato, prosciutto, and cheese. Seasoned with
                  salt and olive oil."/>
